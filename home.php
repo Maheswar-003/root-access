7d7c785a72c9036b22b6fda707830dd4bc5e5676
Maheswar-003 <88921855+Maheswar-003@users.noreply.github.com>
2023-03-16 16:49:08 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index2.html');
+	exit;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+    <body>
+        <p> Hoi</p>
+    </body>
+</html>
